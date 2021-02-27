bb548cd3d8ccfa156fe7533128cae6901b1b5367
Warriorroq <Warriorroq@gmail.com>
2021-02-27 11:00:26 +0200

    Stack

--- stac/stac/Program.cs
+++ stac/stac/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace stac
 {
     class Program
@@ -10,56 +9,63 @@ namespace stac
             Stack<int> stack = new Stack<int>();
 
             int a = int.Parse(Console.ReadLine());
-
             for(int i = 0; i < a; i++)
             {
                 stack.Push(random.Next(-i, i));
             }
-
             Console.WriteLine(stack.ToString());
         }
-        public class Stack<T> {
+        public class Stack<T> : IStack<T>{
             private T[] array;
             private int size;
             public int Count
             {
                 get => size;
             }
+
             public Stack()
             {
                 size = 0;
                 array = new T[1];
             }
-            public bool isEmpty()
+            public bool IsEmpty()
                 => size == 0;
-            public bool isFull()
+            public bool IsFull()
                 => size == array.Length;
             public T Pop()
             {
-                if (isEmpty()) 
-                    throw new Exception("Empty");    
+                if (IsEmpty())
+                    throw new Exception("Empty");
                 return array[--size];
             }
             public T Peek()
             {
-                if (isEmpty())
+                if (IsEmpty())
                     throw new Exception("Empty");
                 return array[size - 1];
             }
             public void Push(T element)
             {
-                if (isFull())
+                if (IsFull())
                     ReSizeArray();
                 array[size++] = element;
             }
             private void ReSizeArray()
-                =>Array.Resize(ref array, size * 2);            
+                => Array.Resize(ref array, size * 2);
             public override string ToString()
             {
-                if (isEmpty())
+                if (IsEmpty())
                     return "Empty"; //throw new Exception("Empty");
-                return string.Join(',', array).Substring(0, Count * 2 - 1);
+                return string.Join(',', array.ToString()).Substring(0, Count * 2 - 1);
             }
         }
+        public interface IStack<T> {
+            public T Peek();
+            public void Push(T element);
+            public T Pop();
+            public bool IsEmpty();
+
+        }
+
     }
 }
